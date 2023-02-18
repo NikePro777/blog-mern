1604ce575a25dd1ca45cca18ef4a6d3366a58972
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-02-18 09:16:02 +0500

    create post linken with user

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -4,15 +4,15 @@ import { PostFields } from "../utils/post.utils.js";
 const client = new PrismaClient();
 export const create = async (req, res) => {
   try {
-    const doc = new PostFields({
-      title: req.body.title,
-      text: req.body.text,
-      imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
-      user: req.userId,
+    const post = await client.post.create({
+      data: {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl ? req.body.imageUrl : "",
+        tags: req.body.tags.toString(),
+        user: { connect: { id: req.userId } },
+      },
     });
-
-    const post = await client.post.create(doc);
     res.json(post);
   } catch (err) {
     console.log(err);
